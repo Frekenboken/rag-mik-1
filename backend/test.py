3bf22704c2f0a10066bad4ca93727f37d132f5d9
UwerfiU <deseptikon2007@gmail.com>
2025-12-10 18:41:44 +0300

    f

--- backend/test.py
+++ backend/test.py
@@ -1,5 +1,7 @@
 from src.modules.rag_system import rag
 
 query = "Какой максимальный вес стеклоизделия может контроллировать MIK-1?"
-answer = rag.interaction(query, '', k=5)
-print(f'user: {query}\nLLM: {answer[0].content}\nИсточники: {'; '.join(answer[1])}')
+
+rag.semsearch_debug(query, k=5)
+#answer = rag.interaction(query, '', k=5)
+#print(f'user: {query}\nLLM: {answer[0].content}\nИсточники: {'; '.join(answer[1])}')
